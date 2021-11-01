f94ebe2f83e24927f39a814fc6d11c3a81f8bec3
Julianna Chen <45462803+jchenj@users.noreply.github.com>
2021-11-01 08:59:50 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,5 +12,14 @@ app.use(express.json())
 app.use(require("/routes/record"));
 
 // get driver connection
-const 
+const dbo = require("./db/conn");
+
+app.listen(port, () => {
+    // perform db connection when server starts
+    dbo.connectToServer(function (err){
+        if (err) console.error(err);
+    });
+    console.log(`Server running on port: ${port}`);
+});
+
 

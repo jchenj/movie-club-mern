452812e762db8acc11ae8f3d9826d1b690710cc9
Julianna Chen <45462803+jchenj@users.noreply.github.com>
2021-11-14 07:20:45 +0100

    Update create.js

---
+++ client/src/components/create.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+// This will require to npm to install axios;
+import axois from "axios";
+
+export default class Create extends Component {
+    // This is the construcor that stores the data
+    constructor(props) {
+        super(props);
+    }
+}

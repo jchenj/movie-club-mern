23161dd210f0e65b740edfac58d61f3d96e75451
Julianna Chen <45462803+jchenj@users.noreply.github.com>
2021-12-03 21:01:28 +0100

    Update edit.js

--- client/src/components/edit.js
+++ client/src/components/edit.js
@@ -38,25 +38,110 @@ class Edit extends Component {
           });
     }
 
-    // TODO: continue here w/ methods to update state properties
+    // Methods to update state properties
+    onChangeFilmTitle(e) {
+        this.setState({
+            film_title: e.target.value,
+        });
+    }
 
+    onChangeFilmDirector(e) {
+        this.setState({
+            film_director: e.target.value,
+        });
+    }
 
-// Section to display the update form that takes input from user to update the data
-render() {
-    return (
-        <div>
-            <h3 align="center">Update Record</h3>
-            <form onSubmit={this.onSubmit}>
-                <div className="form-group">
-                    <label>Title: </label>
-                    <input
-                      type="text"
-                      className="form-control"
-                      value={this.state.film_title}
-                      onChange={this.onChangeFilmTitle} 
-                    />
-                </div>
-            </form>
-        </div>
-    );
+    onChangeFilmLength(e) {
+        this.setState({
+            film_length: e.target.value,
+        });
+    }
+
+    onChangeFilmYear(e) {
+        this.setState({
+            film_year: e.target.value,
+        });
+    }
+
+    // Function to handle the submissions
+    onSubmit(e) {
+        e.preventDefault();
+        const newEditedFilm = {
+            film_title: this.state.film_title,
+            film_director: this.state.film_director,
+            film_length: this.state.film_length,
+            film_year: this.state.film_year,
+        };
+        console.log(newEditedFilm);
+
+        // Send a post request to update data in the db
+        axios  
+          .post(
+              "http://localhost:5000/update" + this.props.match.params.id,
+              newEditedFilm
+          )
+          .then((res) => console.log(res.data));
+
+        this.props.history.push("/");
+    }
+
+    // Section to display the update form that takes input from user to update the data
+    render() {
+        return (
+            <div>
+                <h3 align="center">Update Record</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Title: </label>
+                        <input
+                        type="text"
+                        className="form-control"
+                        value={this.state.film_title}
+                        onChange={this.onChangeFilmTitle} 
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Director: </label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          value={this.state.film_director}
+                          onChange={this.onChangeFilmDirector}
+                          />
+                    </div>
+                    <div className="form-group">
+                        <label>Length: </label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          value={this.state.film_length}
+                          onChange={this.onChangeFilmLength}
+                          />
+                    </div>
+                    <div className="form-group">
+                        <label>Year: </label>
+                        <input 
+                          type="text"
+                          className="form-control"
+                          value={this.state.film_year}
+                          onChange={this.onChangeFilmYear}
+                          />
+                    </div>
+                    <br/>
+
+                    <div className="form-group">
+                        <input
+                          type="submit"
+                          value="Update Record"
+                          className="btn btn-primary"
+                        />
+                    </div>
+                </form>
+            </div>
+        );
+    }
 }
+
+// Can get access to the history object's properties and closest <Route>'s match via the withRouter higher-order component. Makes it easier to edit records. 
+
+export default withRouter(Edit);

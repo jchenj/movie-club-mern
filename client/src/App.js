4c5ecca9ce00bd407c4a7e64d1cdbbe7f64deb48
Julianna Chen <45462803+jchenj@users.noreply.github.com>
2021-12-08 14:47:26 -0800

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,4 +3,25 @@ import React from "react";
 // Use Route to define different routes of app
 import { Route } from "react-router-dom";
 
-// START HERE
+// Import components needed
+import Navbar from "./components/navbar";
+import Edit from "./components/edit";
+import Create from "./components/create";
+import RecordList from "./components/recordList";
+
+const App = () => {
+    return (
+        <div>
+            <Navbar />
+            <Route exact path="/">
+                <RecordList />
+            </Route>
+            <Route path="/edit/:id" component={Edit} />
+            <Route path="/create">
+                <Create />
+            </Route>
+        </div>
+    );
+};
+
+export default App;
